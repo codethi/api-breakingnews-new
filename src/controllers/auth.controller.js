37ee9a2e6eed99232c09a62cdcb0b126fb32a94a
codethi <docencia.thiago@gmail.com>
2022-09-30 11:32:40 -0300

    #17 - O que é JWT e como utilizar no Express

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 import bcrypt from "bcrypt";
-import { loginService } from "../services/auth.service.js";
+import { loginService, generateToken } from "../services/auth.service.js";
 
 const login = async (req, res) => {
   const { email, password } = req.body;
@@ -17,7 +17,9 @@ const login = async (req, res) => {
       return res.status(404).send({ message: "User or Password not found" });
     }
 
-    res.send("Login ok");
+    const token = generateToken(user.id)
+
+    res.send({token});
   } catch (err) {
     res.status(500).send(err.message);
   }
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,10 @@
 import User from "../models/User.js";
+import jwt from "jsonwebtoken";
 
 const loginService = (email) =>
   User.findOne({ email: email }).select("+password");
 
-export { loginService };
+const generateToken = (id) =>
+  jwt.sign({ id: id }, process.env.SECRET_JWT, { expiresIn: 86400 });
+
+export { loginService, generateToken };
