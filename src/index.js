1bd52bbdcf1e9417b5ad237a7423457f201e6342
codethi <docencia.thiago@gmail.com>
2022-11-06 15:29:46 -0300

    refact: alter swagger route to common js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 import userRoute from "./routes/user.route.js";
 import authRoute from "./routes/auth.route.js";
 import newsRoute from "./routes/news.route.js";
-import swaggerRoute from "./routes/swagger.route.js";
+import swaggerRoute from "./routes/swagger.route.cjs";
 
 dotenv.config();
 
